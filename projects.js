bb3ca51ffb298e7579b525abc6ab6fc69f84a00e
bachjawo11-beep <bachjawo11@gmail.com>
2026-06-02 00:31:13 +0000

    projects.js

--- projects.js
+++ projects.js
@@ -1,1 +1,19 @@
-
+const projects = [
+  {
+name: "Coffee Shop Website",
+image: "coffee-shop.png",
+description: "Modern coffee shop website with online ordering."
+},
+{
+name: "Boutique Website",
+image: "https://images.unsplash.com/photo-1556740749-887f6717d7e4?q=80&w=1200&auto=format&fit=crop"
+},
+{
+name: "Restaurant Website",
+image: "https://images.unsplash.com/photo-1517248135467-4c7edcad34c4?q=80&w=1200&auto=format&fit=crop"
+},
+{
+name: "Real Estate Website",
+image: "https://images.unsplash.com/photo-1560518883-ce09059eeffa?q=80&w=1200&auto=format&fit=crop"
+}
+];
